88aca1a29f23129a05d6531af4fc5e23ec482ed3
Niall Robinson <niall.h.robinson@gmail.com>
2017-02-16 13:42:13 +0000

    added dummy test

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,74 +1,9 @@
 const Alexa = require("alexa-sdk");
-const fetch = require("node-fetch");
-
-const handlers = {
-    "GetLocation": function () {
-        const self = this;
-        const location = self.event.request.intent.slots.location.value;
-        if (!location) {
-            if(!self.attributes.location) {
-                self.emit(":ask", "where?", "where would you like the forecast for?")
-            } else{
-                self.emit("FulfillForecast");
-            }
-        } else {
-            self.attributes["location"] = location;
-            self.emit("FulfillForecast");
-        }
-    },
-    "GetForecast": function () {
-        const self = this;
-        const location = self.event.request.intent.slots.location.value;
-        if (!location) {
-            if(!self.attributes.location) {
-                self.emit(":ask", "where?", "where would you like the forecast for?")
-            } else{
-                self.emit("FulfillForecast");
-            }
-        } else {
-            self.attributes["location"] = location;
-            self.emit("FulfillForecast");
-        }
-    },
-    "FulfillForecast": function () {
-        const self = this;
-        const location = self.attributes.location;
-        if (!location) {
-            self.emit(":ask", "where?", "where would you like the forecast for?")
-        } else {
-            fetch("https://shlmog4lwa.execute-api.eu-west-1.amazonaws.com/dev/datapoint?location=" + encodeURIComponent(location))
-                .then(function (res) {
-                    return res.json();
-                })
-                .then(function (json) {
-                    console.log(json);
-                    self.emit(":tell", json.properties.forecast.text.local)
-                })
-                .catch(function (err) {
-                    console.error(err);
-                    self.emit(":tell", "sorry I've failed you");
-                });
-        }
-    },
-    'AMAZON.HelpIntent': function () {
-        const speechOutput = "Don't panick";
-        const reprompt = "Don't panick";
-        this.emit(':ask', speechOutput, reprompt);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', "Bye");
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', "Bye");
-    },
-    'SessionEndedRequest': function () {
-        this.emit(':tell', "Bye");
-    }
-};
+const forecast = require("forecast");
 
 exports.handler = function (event, context) {
     const alexa = Alexa.handler(event, context);
     alexa.dynamoDBTableName = "alexa-met-office-skill";
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(forecast.handlers);
     alexa.execute();
 };
---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Test lambda input for', function(){
+    describe('Weather forecast', function() {
+        it('should give a real JSON back', function() {
+            assert.equal(1, 1);
+        });
+    });
+});
