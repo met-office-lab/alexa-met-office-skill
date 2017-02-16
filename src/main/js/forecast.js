689efac9ca3b1e6ecb061f508f779c6fd7ff13ac
Niall Robinson <niall.h.robinson@gmail.com>
2017-02-16 13:43:01 +0000

    refactored out weather forecast specfici stuff in preparation for shipping forecast stuff

---
+++ src/main/js/forecast.js
@@ -0,0 +1,66 @@
+const fetch = require("node-fetch");
+
+const handlers = {
+    "GetLocation": function () {
+        const self = this;
+        const location = self.event.request.intent.slots.location.value;
+        if (!location) {
+            if(!self.attributes.location) {
+                self.emit(":ask", "where?", "where would you like the forecast for?")
+            } else{
+                self.emit("FulfillForecast");
+            }
+        } else {
+            self.attributes["location"] = location;
+            self.emit("FulfillForecast");
+        }
+    },
+    "GetForecast": function () {
+        const self = this;
+        const location = self.event.request.intent.slots.location.value;
+        if (!location) {
+            if(!self.attributes.location) {
+                self.emit(":ask", "where?", "where would you like the forecast for?")
+            } else{
+                self.emit("FulfillForecast");
+            }
+        } else {
+            self.attributes["location"] = location;
+            self.emit("FulfillForecast");
+        }
+    },
+    "FulfillForecast": function () {
+        const self = this;
+        const location = self.attributes.location;
+        if (!location) {
+            self.emit(":ask", "where?", "where would you like the forecast for?")
+        } else {
+            fetch("https://shlmog4lwa.execute-api.eu-west-1.amazonaws.com/dev/datapoint?location=" + encodeURIComponent(location))
+                .then(function (res) {
+                    return res.json();
+                })
+                .then(function (json) {
+                    console.log(json);
+                    self.emit(":tell", json.properties.forecast.text.local)
+                })
+                .catch(function (err) {
+                    console.error(err);
+                    self.emit(":tell", "sorry I've failed you");
+                });
+        }
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = "Don't panick";
+        const reprompt = "Don't panick";
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', "Bye");
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', "Bye");
+    },
+    'SessionEndedRequest': function () {
+        this.emit(':tell', "Bye");
+    }
+};
