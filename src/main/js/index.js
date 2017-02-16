14fe0e11fdf70700ac0813ca7653d9ea7529029e
Niall Robinson <niall.h.robinson@gmail.com>
2017-02-16 14:25:10 +0000

    stash

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,6 +2,7 @@ const Alexa = require("alexa-sdk");
 const forecast = require("./forecast");
 
 exports.handler = function (event, context) {
+    console.log("hi")
     const alexa = Alexa.handler(event, context);
     alexa.dynamoDBTableName = "alexa-met-office-skill";
     alexa.registerHandlers(forecast.handlers);
